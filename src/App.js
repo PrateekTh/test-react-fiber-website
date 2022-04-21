80a849ab4214814506238b9257e4accf04a746b8
PrateekTh <PrateekTh@users.noreply.github.com>
2022-04-22 00:58:41 +0530

    Basic Cube Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useRef} from "react";
 import './App.scss';
+
+import {Canvas, useFrame} from 'react-three-fiber'
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(()=>(mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return(
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={[1,1,1]} />
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App"></div>
+    <>
+      <Canvas>
+        <Box />
+      </Canvas>
+    </>
   );
 }
 
