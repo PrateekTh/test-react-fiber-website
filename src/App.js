0d7c425220f2bad6c89614d407dbbd49ce999a0e
PrateekTh <PrateekTh@users.noreply.github.com>
2022-04-22 01:14:19 +0530

    More Cubes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.scss';
 
 import {Canvas, useFrame} from 'react-three-fiber'
 
-const Box = () => {
+const Box = ({position, args, color}) => {
   const mesh = useRef(null);
   useFrame(()=>(mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return(
-    <mesh ref={mesh}>
-      <boxBufferGeometry attach='geometry' args={[1,1,1]} />
-      <meshStandardMaterial attach='material' />
+    <mesh position={position} ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={args} />
+      <meshStandardMaterial attach='material' color= {color} />
     </mesh>
   )
 }
@@ -18,8 +18,12 @@ const Box = () => {
 function App() {
   return (
     <>
-      <Canvas>
-        <Box />
+      <Canvas colorManagement>
+        <ambientLight intensity={.3} /> 
+        <pointLight args={[1,1,1]} />
+        <Box position={[0,0,0]} args={[3,2,1]} color='lightblue'/>
+        <Box position={[3,1,0]} args={[1,1,1]} color='pink'/>
+        <Box position={[-3,0,0]} args={[2,2,1]}color='#55E5BB'/>
       </Canvas>
     </>
   );
