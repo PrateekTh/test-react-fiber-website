6be48731b3636d64b5f63b4f2898be0e224594fb
Prateek Thakur <prateek33thakur@gmail.com>
2022-04-27 00:01:47 +0530

    Model

--- src/components/axe.js
+++ src/components/axe.js
@@ -12,18 +12,4 @@ export default function Model(props) {
   )
 }
 
-useGLTF.preload("../../public/hatchet.gltf"import React, { useRef } from "react"
-import { useGLTF } from "@react-three/drei"
-
-export default function Model(props) {
-  const group = useRef()
-  const { nodes, materials } = useGLTF("../../public/hatchet.gltf")
-  return (
-    <group ref={group} {...props} dispose={null}>
-      <mesh castShadow receiveShadow geometry={nodes.Curve007_1.geometry} material={materials['Material.001']} />
-      <mesh castShadow receiveShadow geometry={nodes.Curve007_2.geometry} material={materials['Material.002']} />
-    </group>
-  )
-}
-
-useGLTF.preload("../../public/hatchet.gltf"))
+useGLTF.preload("../../public/hatchet.gltf")
